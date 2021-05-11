fbb7b2f80684a2272bba7a82991f46276a32e37c
Ubuntu <ubuntu@ip-172-31-3-166.ap-southeast-2.compute.internal>
2021-05-11 05:55:17 +0000

    tute 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  
+  get'/signup', to: 'customers#new'
+  post'/signup', to: 'customers#create'
+
+  get '/login', to: 'users#login'
+
+
+  get 'static_pages/home'
+
+  get '/about', to: 'static_pages#about'
+  
+  get '/contact', to: 'static_pages#contact'
+  
+  root 'static_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
